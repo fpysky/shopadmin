375298ce7804e9645ff8dd9b2c051461fd1bdda5
fengpengyuan <fengpengyuan@live.com>
2018-09-28 17:38:19 +0800

    代码规范强制提交测试

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export default{
+  passport: {
+    grant_type: 'password',
+    client_id: 2,
+    client_secret: 'CHCZspEuFcwHwFaEdJzzmSavfNWVVtpRxbkaMgzi',
+    scope: '',
+    provider: 'adminers'
+  }
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,39 +1,43 @@
 <template>
-  <div class="login-container">
-    <el-form class="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left">
-      <div class="title-container">
-        <h3 class="title">{{$t('login.title')}}</h3>
-        <lang-select class="set-language"></lang-select>
-      </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container svg-container_login">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" :placeholder="$t('login.username')"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input name="password" :type="passwordType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
-          :placeholder="$t('login.password')" />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon icon-class="eye" />
-        </span>
-      </el-form-item>
-
-      <div style="margin-bottom:22px;">
-          <div id="captcha" ref="captcha">
-              <p id="wait" v-if="isloading" class="show">正在加载验证码......</p>
+  <div id="container" class="login-container">
+    <div id="output">
+      <div v-show="formBoxShow" class="login-form animated" ref="formBox">
+        <el-form  autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left">
+          <div class="title-container">
+            <h3 class="title">{{$t('login.title')}}</h3>
+            <lang-select class="set-language"></lang-select>
           </div>
+
+          <el-form-item prop="username">
+            <span class="svg-container svg-container_login">
+              <svg-icon icon-class="user" />
+            </span>
+            <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" :placeholder="$t('login.username')"
+            />
+          </el-form-item>
+
+          <el-form-item prop="password">
+            <span class="svg-container">
+              <svg-icon icon-class="password" />
+            </span>
+            <el-input name="password" :type="passwordType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
+              :placeholder="$t('login.password')" />
+            <span class="show-pwd" @click="showPwd">
+              <svg-icon icon-class="eye" />
+            </span>
+          </el-form-item>
+
+          <div style="margin-bottom:22px;">
+              <div id="captcha" ref="captcha">
+                  <p id="wait" v-if="isloading" class="show">正在加载验证码......</p>
+              </div>
+          </div>
+
+          <el-button type="primary" style="width:100%;margin-bottom:30px;opacity:.87;font-size:16px;" :loading="loading" @click.native.prevent="handleLogin">{{$t('login.logIn')}}</el-button>
+        </el-form>
       </div>
-
-      <el-button type="primary" style="width:100%;margin-bottom:30px;" :loading="loading" @click.native.prevent="handleLogin">{{$t('login.logIn')}}</el-button>
-    </el-form>
-
+      
+    </div>
     <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog" append-to-body>
       {{$t('login.thirdpartyTips')}}
       <br/>
@@ -41,16 +45,17 @@
       <br/>
       <social-sign />
     </el-dialog>
-
   </div>
 </template>
-
 <script>
 import { isvalidUsername } from '@/utils/validate'
 import LangSelect from '@/components/LangSelect'
 import SocialSign from './socialsignin'
 import '@/gt'
 import { geetest_api_v1, valiGeet } from '@/api/login'
+import config from '@/config/config'
+import Victor from '@/library/vector'
+import animate from 'animate.css'
 
 export default {
   components: { LangSelect, SocialSign },
@@ -76,76 +81,87 @@ export default {
         password: ''
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur'}],//validator: validateUsername
-        password: [{ required: true, trigger: 'blur'}]//validator: validatePassword
+        username: [{ required: true, trigger: 'blur' }], // validator: validateUsername
+        password: [{ required: true, trigger: 'blur' }]// validator: validatePassword
       },
       passwordType: 'password',
       loading: false,
       showDialog: false,
-      isloading:false,
-      status:0,
-      isValidate:false
+      isloading: false,
+      status: 0,
+      isValidate: false,
+      formBoxShow: false
     }
   },
+  created() {
+    this._initGeetest()
+  },
+  mounted() {
+    this.$nextTick(() => {
+      new Victor('container', 'output')
+    })
+  },
   methods: {
-    _initGeetest(){
-      let _this = this
-      this.initGeetest(function(captchaObj){
-        //验证成功
-        captchaObj.onSuccess(function () {
-            _this.isdisabled = true;
-            _this.value = "正在登录中...";
-            _this.loading = true;
-            let validate = captchaObj.getValidate();
-            if (!validate) {
-                _this.$Message.error("请先完成验证");
-                return;
-            }
-            // _this.loginForm.geetest_challenge = validate.geetest_challenge
-            // _this.loginForm.geetest_seccode = validate.geetest_seccode
-            // _this.loginForm.geetest_validate = validate.geetest_validate
-            let params = {
-              geetest_challenge : validate.geetest_challenge,
-              geetest_seccode : validate.geetest_seccode,
-              geetest_validate : validate.geetest_validate,
-              geetest_status : _this.status
-            }
-            _this.valiGeet(params)
-        });
-        captchaObj.onError(function () {
+    _initGeetest() {
+      const _this = this
+      this.initGeetest(function(captchaObj) {
+        captchaObj.onSuccess(function() {
+          _this.isdisabled = true
+          _this.value = '正在登录中...'
+          _this.loading = true
+          const validate = captchaObj.getValidate()
+          if (!validate) {
+            _this.$message.error('请先完成验证')
+            return
+          }
+          const params = {
+            geetest_challenge: validate.geetest_challenge,
+            geetest_seccode: validate.geetest_seccode,
+            geetest_validate: validate.geetest_validate,
+            geetest_status: _this.status
+          }
+          _this.valiGeet(params)
+        })
+        captchaObj.onError(function() {
           _this.$refs.captcha.innerHTML = ''
           _this.isdisabled = false
           _this.value = '登录'
-        });
-        captchaObj.onReady(function () {
+        })
+        captchaObj.onReady(function() {
           _this.isdisabled = true
-          _this.value = "请完成验证";
-        });
-          _this.$refs.captcha.innerHTML = ''
-        captchaObj.appendTo("#captcha");
+          _this.value = '请完成验证'
+        })
+        _this.$refs.captcha.innerHTML = ''
+        captchaObj.appendTo('#captcha')
       })
     },
-    valiGeet(params){
+    valiGeet(params) {
       valiGeet(params).then(res => {
         this.isValidate = true
         this.handleLogin()
-      });
+      })
     },
-    initGeetest(obj){
+    initGeetest(obj) {
       geetest_api_v1().then(res => {
-            var data = res.data;
-            this.status = data.status;
-            initGeetest({
-                // 以下 4 个配置参数为必须，不能缺少
-                gt: data.gt,
-                challenge: data.challenge,
-                offline: !data.success, // 表示用户后台检测极验服务器是否宕机
-                new_captcha: data.new_captcha, // 用于宕机时表示是新验证码的宕机
+        var data = res.data
+        this.status = data.status
+        initGeetest({
+          // 以下 4 个配置参数为必须，不能缺少
+          gt: data.gt,
+          challenge: data.challenge,
+          offline: !data.success, // 表示用户后台检测极验服务器是否宕机
+          new_captcha: data.new_captcha, // 用于宕机时表示是新验证码的宕机
 
-                product: "float", // 产品形式，包括：float，popup
-                width: "448px"
-            }, obj);
+          product: 'float', // 产品形式，包括：float，popup
+          width: '448px'
+        }, obj)
+        this.$nextTick(() => {
+          setTimeout(() => {
+            this.$refs.formBox.classList.add('zoomIn')
+            this.formBoxShow = true
+          }, 200)
         })
+      })
     },
     showPwd() {
       if (this.passwordType === 'password') {
@@ -157,24 +173,23 @@ export default {
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
-          if(!this.isValidate){
+          if (!this.isValidate) {
             this.$message.warning('请滑动以完成验证')
             return
           }
           this.loginForm.username = this.loginForm.username.trim()
           this.loginForm.password = this.loginForm.password.trim()
-          this.loginForm.grant_type = 'password'
-          this.loginForm.client_id = 2
-          this.loginForm.client_secret = 'CHCZspEuFcwHwFaEdJzzmSavfNWVVtpRxbkaMgzi'
-          this.loginForm.scope = ''
-          this.loginForm.provider = 'adminers'
+          this.loginForm.grant_type = config.passport.grant_type
+          this.loginForm.client_id = config.passport.client_id
+          this.loginForm.client_secret = config.passport.client_secret
+          this.loginForm.scope = config.passport.scope
+          this.loginForm.provider = config.passport.provider
           this.loading = true
           this.$store.dispatch('LoginByUsername', this.loginForm).then(() => {
             this.loading = false
-            this.$router.push({ path: '/' })////登录成功之后重定向到首页
+            this.$router.push({ path: '/' })
           }).catch(error => {
             this.loading = false
-            // this.$message.warning(error.response.data.message)
             this.$message.warning('用户名或密码错误')
             this._initGeetest()
             this.isValidate = false
@@ -184,36 +199,11 @@ export default {
           return false
         }
       })
-    },
-    afterQRScan() {
-      // const hash = window.location.hash.slice(1)
-      // const hashObj = getQueryObject(hash)
-      // const originUrl = window.location.origin
-      // history.replaceState({}, '', originUrl)
-      // const codeMap = {
-      //   wechat: 'code',
-      //   tencent: 'code'
-      // }
-      // const codeName = hashObj[codeMap[this.auth_type]]
-      // if (!codeName) {
-      //   alert('第三方登录失败')
-      // } else {
-      //   this.$store.dispatch('LoginByThirdparty', codeName).then(() => {
-      //     this.$router.push({ path: '/' })
-      //   })
-      // }
     }
-  },
-  created() {
-    // window.addEventListener('hashchange', this.afterQRScan)
-    this._initGeetest()
-  },
-  destroyed() {
-    // window.removeEventListener('hashchange', this.afterQRScan)
   }
+
 }
 </script>
-
 <style rel="stylesheet/scss" lang="scss">
   /* 修复input 背景不协调 和光标变色 */
   /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
@@ -329,5 +319,20 @@ $light_gray:#eee;
     right: 35px;
     bottom: 28px;
   }
+}
+</style>
+<style>
+*{margin: 0;padding: 0;}
+#container {
+	position: absolute;
+	height: 100%;
+	width: 100%;
+}
+#output {
+	width: 100%;
+	height: 100%;
+}
+.login-form {
+  animation-duration: .8s;
 }
 </style>
